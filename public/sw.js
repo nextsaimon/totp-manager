23fbd5da2cb80951e1bd40c9e29f9ac02335feb5
Saimon <saimonss5432@gmail.com>
2025-11-10 08:34:51 +0600

    ok

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,8 @@
-// This is the service worker with the offline-first behavior
-// The Cache Storage API is used to cache resources for offline use
-// This code is handled by next-pwa, no need to register it manually
 self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open("offline-cache").then(function (cache) {
       return cache.addAll([
-        "/", // Cache the main page
+        "/",
         "/manifest.json",
         "/icons/icon-512x512.png",
       ]);
